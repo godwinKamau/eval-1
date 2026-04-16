c96e8fd891733d2b58f2129b0d4d584b555f6a87
Godwin Kamau <contactgodwinkamau@gmail.com>
2026-04-16 12:15:01 -0400

    changed row limit

--- evalToBraintrust.ts
+++ evalToBraintrust.ts
@@ -11,7 +11,7 @@ async function main() {
 
   const dataset = initDataset("My Project", { dataset: "Evaluation Dataset" });
 
-  const maxRows = 10;
+  const maxRows = 7;
   let row = 0;
   for await (const item of dataset) {
     if (row >= maxRows) break;
